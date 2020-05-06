7def6c3471d4aa9948f71df3a55ee4748f93d951
yuyurun <yuyu1.run@icloud.com>
2020-05-06 17:16:06 +0900

    日ごとに集計

--- convert.py
+++ convert.py
@@ -5,6 +5,7 @@ import argparse
 import pandas as pd
 from pandas import DataFrame
 from lxml import objectify
+import os
 
 
 def convert_to_df(filename):
@@ -29,11 +30,25 @@ def extract_data(df,typename):
     '''
 
     exdf = df[df['type'] == typename]
+    exdf['value'] = exdf['value'].astype(float)
     
     print(exdf)
     print(len(exdf))
+    
+    exdf.to_csv(args.output_path + os.path.basename(os.path.splitext(args.input_path)[0]) + '_step.csv')
+    return exdf
 
+def daily_count(df):
+    '''
+    日ごとに集計xxx
+    '''
 
+    dfstep = DataFrame({'step': df['value'].tolist()},index=df['endDate'])
+    dfstep.index = pd.to_datetime(dfstep.index, utc=True)
+    dfstep['datetime'] = dfstep.index.tz_convert('Asia/Tokyo')
+    dfstep['date'] = dfstep['datetime'].dt.date
+    sumdf = dfstep.groupby(['date']).sum()
+    sumdf.to_csv(args.output_path + os.path.basename(os.path.splitext(args.input_path)[0]) + '_sumstep.csv')
 
     
 
@@ -41,7 +56,9 @@ def extract_data(df,typename):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i','--input_path',help='input path (.xml)')
+    parser.add_argument('-o','--output_path',help='output path (/)')
     args = parser.parse_args()
 
     df = convert_to_df(args.input_path)
-    extract_data(df, 'HKQuantityTypeIdentifierStepCount')
+    stepdf = extract_data(df, 'HKQuantityTypeIdentifierStepCount')
+    daily_count(stepdf)
