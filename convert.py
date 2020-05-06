fc7b44d9c3deb12fe9c1dc9bff71fe1c9214b867
yuyurun <yuyu1.run@icloud.com>
2020-05-06 14:02:58 +0900

    特定のデータ取得

--- convert.py
+++ convert.py
@@ -6,7 +6,12 @@ import pandas as pd
 from pandas import DataFrame
 from lxml import objectify
 
-def main(filename):
+
+def convert_to_df(filename):
+    '''
+    xml fileをdfに格納
+    '''
+
     headers = ['type','unit','startDate','endDate','value']
     parsed = objectify.parse(open(filename))
     root = parsed.getroot()
@@ -14,8 +19,22 @@ def main(filename):
     data = [({k: v for k, v in elem.attrib.items() if k in headers})
             for elem in root.Record]
     df = DataFrame(data)
-    print(df.head())
-    print(len(df))
+    print('\n'.join(df['type'].unique()))
+    print('取得したデータ数: {}'.format(len(df)))
+    return df
+
+def extract_data(df,typename):
+    '''
+    特定のデータだけを取得
+    '''
+
+    exdf = df[df['type'] == typename]
+    
+    print(exdf)
+    print(len(exdf))
+
+
+
     
 
 
@@ -24,4 +43,5 @@ if __name__ == '__main__':
     parser.add_argument('-i','--input_path',help='input path (.xml)')
     args = parser.parse_args()
 
-    main(args.input_path)
+    df = convert_to_df(args.input_path)
+    extract_data(df, 'HKQuantityTypeIdentifierStepCount')
